b63d9e035963e0c56b15ce29818deaf362871394
fanf <fanf@fanf>
2023-01-24 00:38:02 +0100

    a

--- sample.py
+++ sample.py
@@ -11,12 +11,12 @@ from model import GPTConfig, GPT
 # -----------------------------------------------------------------------------
 out_dir = 'out'
 start = "\n" # or "<|endoftext|>" or whatever you like
-num_samples = 10 # number of samples to draw
-max_new_tokens = 500 # number of tokens generated in each sample
+num_samples = 1 # number of samples to draw
+max_new_tokens = 40 # number of tokens generated in each sample
 temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
 seed = 1337
-device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
+device = 'cpu' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 dtype = 'bfloat16' # 'float32' or 'bfloat16' or 'float16'
 compile = False # use PyTorch 2.0 to compile the model to be faster
 exec(open('configurator.py').read()) # overrides from command line or config file
@@ -75,5 +75,6 @@ with torch.no_grad():
     with ctx:
         for k in range(num_samples):
             y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
-            print(decode(y[0].tolist()))
+            res = decode(y[0].tolist())
+            print(res)
             print('---------------')
