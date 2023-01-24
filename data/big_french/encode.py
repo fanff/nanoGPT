0b401ef70b6eb68b0136541d4a506842e58dcab2
fanf <fanf@fanf>
2023-01-25 00:04:26 +0100

    a

--- data/big_french/encode.py
+++ data/big_french/encode.py
@@ -17,22 +17,32 @@ import os
 def main(input_file, encoder):
     enc = tiktoken.get_encoding(encoder)
 
-    data = []
-    try:
-        with open(input_file, 'r', encoding="utf-8") as f:
-            while True:
-                a = f.read(1000)
-                if len(a)==0: break
-                data.append(a)
-    except Exception as e:
-        print(e)
-    data = "".join(data)
-    print(f"data has {len(data):,} chars")
 
-    toks = np.array(enc.encode_ordinary(data), dtype=np.uint16)
-    print(f"data has {len(toks):,} tokens")
+    with open(input_file, 'r', encoding="utf-8") as f:
+        stack = []
+        stack_size = 0
+        outidx=0
+        while True:
+            a = f.read(1000000)
+            if len(a) == 0:
+                write_stack(stack,stack_size,outidx)
 
-    toks.tofile(input_file+'.bin')
+                break
+
+            toks = np.array(enc.encode_ordinary(a), dtype=np.uint16)
+
+            stack.append(toks)
+            stack_size+=len(toks)
+
+            if stack_size>=200*1000*1000:
+                write_stack(stack,stack_size,outidx)
+                stack = []
+                outidx+=1
+                stack_size=0
+def write_stack(stack,stack_size,idx):
+    filename = f"{idx:04d}.bin"
+    np.concatenate(stack).tofile(filename)
+    print(f"wrote {filename} with {stack_size} tokens")
 
 if __name__ == '__main__':
     main()
